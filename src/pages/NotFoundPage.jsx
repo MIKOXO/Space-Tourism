4c1eaa271a33904f4cc83625c98b3032691564e7
MIKOXO <mikermount19@yahoo.com>
2025-09-04 17:24:48 +0300

    Not found 404 page added

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <section className="font-Barlow mt-20 text-center flex flex-col justify-center items-center h-96">
+      <FaExclamationTriangle className="text-yellow-400 text-6xl mb-4" />
+      <h1 className="text-6xl font-bold mb-4">404 Not Found</h1>
+      <p className="text-xl mb-5">This page does not exist</p>
+      <Link to="/" className="text-white bg-Blue900 rounded-md px-3 py-2 mt-4">
+        Go Back
+      </Link>
+    </section>
+  );
 };
 
 export default NotFoundPage;
